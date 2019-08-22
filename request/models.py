6b5d8b7ff24d665b7662165d4c09e40806951f5f
Alexander Popelyuk <alexander@popelyuk.net>
2019-08-22 21:52:18 +0300

    something

--- request/models.py
+++ request/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Person(models.Model):
+  objects = models.Manager()
   name = models.CharField(max_length=32)
   age = models.IntegerField()
   
@@ -8,6 +9,7 @@ class Person(models.Model):
     return self.name
 
 class Company(models.Model):
+  objects = models.Manager()
   name = models.CharField(max_length=32)
   manager = models.ForeignKey(Person, on_delete=models.CASCADE)
   
@@ -15,6 +17,7 @@ class Company(models.Model):
     return self.name
   
 class Vehicle(models.Model):
+  objects = models.Manager()
   name = models.CharField(max_length=32)
   color = models.CharField(max_length=16)
   company = models.ForeignKey(Company, on_delete=models.CASCADE)
@@ -23,12 +26,14 @@ class Vehicle(models.Model):
     return self.name
 
 class Place(models.Model):
+  objects = models.Manager()
   name = models.CharField(max_length=32)
   
   def __str__(self):
     return self.name
 
 class Trip(models.Model):
+  objects = models.Manager()
   vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
   start = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='start_trips_set')
   target = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='target_trips_set')
@@ -37,6 +42,7 @@ class Trip(models.Model):
     return "%s => %s" % (self.start.name, self.target.name)
   
 class TripParams(models.Model):
+  objects = models.Manager()
   min_allowed_speed = models.FloatField()
   max_allowed_speed = models.FloatField()
   allowed_passengers_count = models.IntegerField()
@@ -49,13 +55,14 @@ class TripParams(models.Model):
     )
 
 class TripInterval(models.Model):
+  objects = models.Manager()
   timestamp = models.DateTimeField(auto_now=True)
   params = models.ForeignKey(TripParams, on_delete=models.CASCADE)
   trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
-  path = models.FloatField()
   time = models.FloatField()
+  path = models.FloatField()
   
   def __str__(self):
-    return "%s: %s, (%s), path=%.3f, time=%.3f" % (
-      self.timestamp, self.params.driver.name, self.trip, self.path, self.time
+    return "%s: %s, (%s), time=%.3f, path=%.3f" % (
+      self.timestamp, self.params.driver.name, self.trip, self.time, self.path
     )
--- request/views.py
+++ request/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import (
-  F, Q, When, Case, Value, FloatField, IntegerField
+  F, Q, When, Case, Value, FloatField, IntegerField,
+  Sum, Avg,
 )
 from .models import (
   Person, Company, Vehicle, Place, Trip, TripParams, TripInterval,
@@ -13,12 +14,28 @@ class RequestView(TemplateView):
   template_name = "request.html"
   
   def get_context_data(self, **kwargs):
-    self.extra_context = dict(results=json.dumps(self.get_result(), indent=2))
+    self.extra_context = dict(results=json.dumps(self.get_result(), indent=2, default=str))
     return super().get_context_data(**kwargs)
   
   def get_result(self):
     result = {
       "this": 'is working variant of project'
     }
+    queryset = TripInterval.objects.annotate(
+      tm=F('timestamp'),
+      spd=(F('path') / F('time')),
+      drv=F('params__driver__name'),
+      big_spd=Case(
+        When(spd__gt=1, then=F('spd')),
+        output_field=FloatField()
+      ),
+    ).aggregate(
+      spd_sum=Sum('spd'),
+      big_spd_sum=Sum('big_spd')
+    )
+    
+    print(queryset)
+    
+    result['objects'] = list(queryset)
     
     return result
