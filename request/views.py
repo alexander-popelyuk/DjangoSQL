d990a3f515781c6a10491919e2f2a0f3b404e93a
Alexander Popelyuk <alexander@popelyuk.net>
2019-08-22 22:23:30 +0300

    more stuff

--- request/views.py
+++ request/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import (
   F, Q, When, Case, Value, FloatField, IntegerField,
-  Sum, Avg,
+  Sum, Avg, Count
 )
 from .models import (
   Person, Company, Vehicle, Place, Trip, TripParams, TripInterval,
@@ -14,28 +14,31 @@ class RequestView(TemplateView):
   template_name = "request.html"
   
   def get_context_data(self, **kwargs):
-    self.extra_context = dict(results=json.dumps(self.get_result(), indent=2, default=str))
+    result = self.get_result()
+    self.extra_context = dict(results=json.dumps(result, indent=2, default=str))
     return super().get_context_data(**kwargs)
   
   def get_result(self):
-    result = {
-      "this": 'is working variant of project'
-    }
     queryset = TripInterval.objects.annotate(
-      tm=F('timestamp'),
-      spd=(F('path') / F('time')),
-      drv=F('params__driver__name'),
-      big_spd=Case(
+      **self.get_annotation()
+    ).aggregate(
+      spd_sum=Sum('spd'),
+      big_spd_sum=Sum('big_spd'),
+      spd_cnt=Count('spd'),
+      big_spd_cnt=Count('big_spd')
+    )
+    
+    result = dict(queryset)
+    
+    return result
+
+  def get_annotation(self):
+    return {
+      'tm': F('timestamp'),
+      'spd':(F('path') / F('time')),
+      'drv': F('params__driver__name'),
+      'big_spd': Case(
         When(spd__gt=1, then=F('spd')),
         output_field=FloatField()
       ),
-    ).aggregate(
-      spd_sum=Sum('spd'),
-      big_spd_sum=Sum('big_spd')
-    )
-    
-    print(queryset)
-    
-    result['objects'] = list(queryset)
-    
-    return result
+    }
